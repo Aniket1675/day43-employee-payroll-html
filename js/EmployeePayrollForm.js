ff7ecac6a8b9637c6d9273fa7eec7b6f6291b262
Aniket1675 <Aniket@05>
2022-05-06 15:27:58 +0530

    ADDED: Employee Payroll System Home Page UI

--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -1,14 +1,33 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    salaryOutput();
-
+let employeePayrollList;
+window.addEventListener("DOMContentLoaded", () => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+    
 });
 
-function salaryOutput() {
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salary.addEventListener('input', function() {
-        output.textContent = salary.value;
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem("EmployeePayrollList") ?
+        JSON.parse(localStorage.getItem("EmployeePayrollList")) : [];
+};
 
-    });
-}
+
+
+const getDepartmentHtml = (departmentList) => {
+    let departmentHtml = "";
+    for (let department of departmentList) {
+        departmentHtml = `${departmentHtml} <div class="dept-label">${department}</div>`;
+    }
+    return departmentHtml;
+};
+
+const remove = (node) => {
+    let employeePayrollData = employeePayrollList.find(employeeData => employeeData._id == node.id);
+    if (!employeePayrollData) return;
+    const index = employeePayrollList.map(employeeData => employeeData._id).indexOf(employeePayrollData._id);
+    employeePayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
+    createInnerHtml();
+};
